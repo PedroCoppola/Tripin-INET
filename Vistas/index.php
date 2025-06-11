99243065c76840c009e71162710fcbf0ddcb2f05
Marcos22121 <marcosvillaro007@gmail.com>
2025-06-11 20:54:04 -0300

    Añadi el index y un poco dl diseño ahora hago mas

---
+++ Vistas/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TravelEase</title>
+    <link rel="stylesheet" href="../Estilos/style_index.css" />
+</head>
+<body>
+    <header class="banner">
+        <div class="banner-content">
+            <h1>¡Explora el mundo con Trippin!</h1>
+            <p> Descubre paquetes de viaje seleccionados a los destinos más impresionantes. Tu aventura te espera.</p>
+            <button>Encuentra un viaje</button>
+        </div>
+    </header>
+
+   <section class="featured">
+    <h2>Featured Trips</h2>
+    <div class="card-row">
+        <div class="card">
+            <img src="../img/featured/paris.jpg" alt="Featured1">
+            <h1>Paris</h1>
+            <p>Viaje a paris</p>
+        </div>
+        <div class="card">
+            <img src="../img/featured/serrascord.jpg" alt="Featured2">
+            <h1>Sierras De Córdoba</h1>
+            <p>Este es otro viaje pero en vez de a maldivia a las sierras de cordoba papa</p>
+        </div>
+        <div class="card">
+            <img src="../img/featured/maldivas.jpg" alt="Featured3">
+            <h1>Viaje a las maldivas re copado</h1>
+            <p>Este es un viaje a las maldivas y si no te gusta comeme bien la banana</p>
+        </div>
+    </div>
+</section>
+
+    <section class="popular">
+        <h2>Popular Destinations</h2>
+        <div class="grid">
+            <div class="grid-item">Paris</div>
+            <div class="grid-item">Rome</div>
+            <div class="grid-item">New York</div>
+            <div class="grid-item">Tokyo</div>
+            <div class="grid-item">Sydney</div>
+            <div class="grid-item">Rio de Janeiro</div>
+        </div>
+    </section>
+</body>
+</html>
